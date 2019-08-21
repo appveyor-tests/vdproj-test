7ed3280a54c4f6ea9e70aebdd5d15c125cab5495
Alena Hrynkevich <alena@appveyor.com>
2019-08-20 17:42:17 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 appveyor version
 cmd /c 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com' SetupProjectTestApp.sln /build Debug /project Setup1\Setup1.vdproj /projectconfig Debug /Out errorFile.txt
 #If some errors occurred during migration. For more information, see the migration report UpgradeLog.htm
-env:APPVEYOR_BUILD_FOLDER
-if (test-path "env:APPVEYOR_BUILD_FOLDER\UpgradeLog.htm") { Push-AppVeyorArtifact  env:APPVEYOR_BUILD_FOLDER\UpgradeLog.htm}
+$env:APPVEYOR_BUILD_FOLDER
+if (test-path "$env:APPVEYOR_BUILD_FOLDER\UpgradeLog.htm") { Push-AppVeyorArtifact  $env:APPVEYOR_BUILD_FOLDER\UpgradeLog.htm}
