cbbd9e4385652fdbca9b16393b4de15c49f31689
Alena Hrynkevich <alena@appveyor.com>
2019-08-20 17:29:32 -0700

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,3 @@
+appveyor version
+
+cmd /c 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com' SetupProjectTestApp.sln /build Debug /project Setup1\Setup1.vdproj /projectconfig Debug /Out errorFile.txt
